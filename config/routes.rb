3c5c347aa54bd5765ce2d978d96baf8a670cd68d
Joseph Quattrone <josephquattrone@Josephs-MBP.fios-router.home>
2018-11-13 15:26:06 -0500

    added draft/published toggle for Blogs

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   get '/about'   => 'pages#about'
   get '/contact' => 'pages#contact'
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
