d62a3565beb59e7faece48dc256a01f26c8c27e0
Joseph Quattrone <josephquattrone@Josephs-MBP.fios-router.home>
2018-11-14 14:30:13 -0500

    Added technologies feature to portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ puts "5 skills created"
 
 8.times do |portfolio_item|
   Portfolio.create!(
-    title: "Potfolio title #{portfolio_item}",
+    title: "Portfolio title #{portfolio_item}",
     subtitle: "Ruby on Rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://via.placeholder.com/600x400",
@@ -45,10 +45,18 @@ puts "8 portfolio items created"
 
 1.times do |portfolio_item|
   Portfolio.create!(
-    title: "Potfolio title #{portfolio_item}",
+    title: "Portfolio title #{portfolio_item}",
     subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200",
   )
 end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
