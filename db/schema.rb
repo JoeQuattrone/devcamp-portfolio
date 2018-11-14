62f1d044bd5554fd2c6b1aa8310585431ced4a5f
Joseph Quattrone <josephquattrone@Josephs-MBP.fios-router.home>
2018-11-14 16:09:33 -0500

    Created helper method in concerns to generate a placeholder image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_185816) do
+ActiveRecord::Schema.define(version: 2018_11_14_205214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_185816) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "technologies", force: :cascade do |t|
