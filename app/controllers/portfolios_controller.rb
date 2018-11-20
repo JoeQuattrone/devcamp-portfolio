f3a63d3bf05ef3265b6a6b048ee7d091cbb968d1
Joseph Quattrone <josephquattrone@Josephs-MBP.home>
2018-11-20 15:29:37 -0500

    Implemented partials with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -34,7 +34,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(portfolio_params)
+      if @portfolio_item.update(portfolios_params)
         format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
       else
         format.html { render :edit }
